dcc0ebb8c75d7eb960bc1e5f7b2d1c6f5dd39f73
Bulbul Kumari <kbulbul026@gmail.com>
2026-08-09 17:16:40 +0530

    Update urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('add-customer/', views.add_customer, name='add_customer'),
     path('edit-customer/<int:customer_id>/', views.edit_customer, name='edit_customer'),
     path('delete-customer/<int:customer_id>/', views.delete_customer, name='delete_customer'),
-    
+
     path('entry/<int:customer_id>/', views.add_transaction, name='add_transaction'),
     path('edit-transaction/<int:transaction_id>/', views.edit_transaction, name='edit_transaction'),
     path('delete-transaction/<int:transaction_id>/', views.delete_transaction, name='delete_transaction'),
-    
+
     path('customer/<int:customer_id>/', views.customer_detail, name='customer_detail'),
     path('customer/<int:customer_id>/print/', views.print_customer, name='print_customer'),
-    
+
     path('export-csv/', views.export_csv, name='export_csv'),
 ]
